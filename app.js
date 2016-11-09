25a266ca7ecfdbef39a52fe23c53e6a22b9b779b
SmithingChick <sharon.smith.metals@gmail.com>
2016-11-09 14:16:06 -0800

    new game page, more html content,headings, css tweaks inc hammer rotate

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
 'use strict';
 
-
-console.log('Hello World!');
-
+// Greetings
 var userName = prompt('Welcome! What\'s your name?');
-console.log('User name is ' + userName);
-
+console.log('Your name is ' + userName);
 alert('Greetings, ' + userName + '. Let\'s see how well you know Sharon.');
 console.log('Would you like to play a game?');
 
+// Questions
 var sarcasmMuch = prompt('Is Sharon sarcastic?');
-console.log('It\'s her first language.');
-
 if (sarcasmMuch.toLowerCase() === 'yes' || sarcasmMuch.toLowerCase() === 'y' ) {
   alert('So you\'ve met her, then.');
 } else if (sarcasmMuch.toLowerCase() === 'no' || sarcasmMuch.toLowerCase() === 'n' ){
-  alert('Clearly, you\'ve never had a conversation with her, ' + userName + ' She\'s a punk rock chick from the Rust Belt. It\'s her first language.');
+  alert('Clearly, you\'ve never had a conversation with her, ' + userName + '. She\'s a punk rock chick from the Rust Belt. I don\'t think they\'re allowed to speak without a certain level of sarcasm.');
 } else {
   alert('Dude! WTF is that? Yes or no. It\'s not that complicated');
 }
-
+console.log('It\'s her first language.');
 
 var blackBirds = prompt('Does Sharon like large black birds like crows & ravens?');
-console.log('*starts humming the Beatle\'s "Blackbird"*');
-
 if (blackBirds.toLowerCase() === 'yes' || blackBirds.toLowerCase() === 'y' ) {
   alert('That\'s right. She regards them as something of a spirit animal.');
 } else if (blackBirds.toLowerCase() === 'no' || blackBirds.toLowerCase() === 'n' ){
@@ -31,23 +25,19 @@ if (blackBirds.toLowerCase() === 'yes' || blackBirds.toLowerCase() === 'y' ) {
 } else {
   alert('Dude! WTF is that? Yes or no. It\'s not that complicated');
 }
-
+console.log('*starts humming the Beatle\'s "Blackbird"*');
 
 var wombatFan = prompt('Does Sharon also love wombats?');
-console.log('May the Wombat of Happiness shuffle through your underbrush.');
-
 if (wombatFan.toLowerCase() === 'yes' || wombatFan.toLowerCase() === 'y' ) {
   alert('Yes indeed, ' + userName + '. She even has 2 stuffed wombats she considers her "pets". Their names are Watson & Wanda');
 } else if (wombatFan.toLowerCase() === 'no' || wombatFan.toLowerCase() === 'n' ){
   alert('Oh, she does, ' + userName + '. She does. It\'s all North Dakota\'s fault.');
 } else {
-  alert('Dude! WTF is that? Yes or no. It\'s not that complicated');
+  alert('Dude! WTF is that? Yes or no. It\'s not that complicated.');
 }
-
+console.log('May the Wombat of Happiness shuffle through your underbrush.');
 
 var radioDJ = prompt('Was Sharon a college radio DJ?');
-console.log('This request goes out to my boys at the BP on I-90');
-
 if (radioDJ.toLowerCase() === 'yes' || radioDJ.toLowerCase() === 'y' ) {
   alert('Did you ever hear her show, ' + userName + '?');
 } else if (radioDJ.toLowerCase() === 'no' || radioDJ.toLowerCase() === 'n' ){
@@ -55,11 +45,9 @@ if (radioDJ.toLowerCase() === 'yes' || radioDJ.toLowerCase() === 'y' ) {
 } else {
   alert('Dude! WTF is that? Yes or no. It\'s not that complicated');
 }
-
+console.log('This request goes out to my boys at the BP on I-90');
 
 var heavyReader = prompt('Does Sharon hate to read?');
-console.log('I like big books and I cannot lie.');
-
 if (heavyReader.toLowerCase() === 'yes' || heavyReader.toLowerCase() === 'y' ) {
   alert('Wrong! Wrong! Wrong! You\'ve obviously never been to her apartment, ' + userName + '.');
 } else if (heavyReader.toLowerCase() === 'no' || heavyReader.toLowerCase() === 'n' ){
@@ -67,3 +55,23 @@ if (heavyReader.toLowerCase() === 'yes' || heavyReader.toLowerCase() === 'y' ) {
 } else {
   alert('Dude! WTF is that? Yes or no. It\'s not that complicated');
 }
+console.log('I like big books and I cannot lie.');
+
+
+// Wednesday Lab Additions
+// Number Guess
+
+var numberGuess = prompt('How many semesters did Sharon teach Jewelry/Metalsmithing at UND?');
+var numberGuessI = parseInt(numberGuess);
+var teachSemesters = 6;
+
+if (numberGuessI === teachSemesters) {
+  alert('That\'s right! She did!');
+} else if (numberGuessI < teachSemesters) {
+  alert('Not quite. She taught more than ' + numberGuess + '.');
+} else if (numberGuessI > teachSemesters) {
+  alert('Alas, Sharon didn\'t teach quite that many');
+} else {
+  alert('That\'s not a number, silly!');
+}
+console.log('You guessed ' + numberGuessI + '. She taught for ' + teachSemesters + ' semesters. She loved it!');
