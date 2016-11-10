cf7a429a3a56b6d5a13bff1b4f9048ca02cbcdff
SmithingChick <sharon.smith.metals@gmail.com>
2016-11-09 16:07:05 -0800

    added question text to game html, added Q7 & correct ans counter

--- app.js
+++ app.js
@@ -3,75 +3,140 @@
 // Greetings
 var userName = prompt('Welcome! What\'s your name?');
 console.log('Your name is ' + userName);
-alert('Greetings, ' + userName + '. Let\'s see how well you know Sharon.');
+alert('Greetings, ' + userName + '. Let\'s see how well you know me.');
 console.log('Would you like to play a game?');
+var totalCorrect = 0;
 
-// Questions
-var sarcasmMuch = prompt('Is Sharon sarcastic?');
-if (sarcasmMuch.toLowerCase() === 'yes' || sarcasmMuch.toLowerCase() === 'y' ) {
-  alert('So you\'ve met her, then.');
-} else if (sarcasmMuch.toLowerCase() === 'no' || sarcasmMuch.toLowerCase() === 'n' ){
-  alert('Clearly, you\'ve never had a conversation with her, ' + userName + '. She\'s a punk rock chick from the Rust Belt. I don\'t think they\'re allowed to speak without a certain level of sarcasm.');
-} else {
-  alert('Dude! WTF is that? Yes or no. It\'s not that complicated');
+// Question 1 - Sarcasm
+function sarcasm() {
+  var sarcasmMuch = prompt('Am I sarcastic?');
+  if (sarcasmMuch.toLowerCase() === 'yes' || sarcasmMuch.toLowerCase() === 'y' ) {
+    alert('So you\'ve met her, then.');
+    totalCorrect++;
+  } else if (sarcasmMuch.toLowerCase() === 'no' || sarcasmMuch.toLowerCase() === 'n' ){
+    alert('Clearly, you\'ve never had a conversation with me, ' + userName + '. I\'m a punk rock chick from the Rust Belt. We\'re not allowed to speak without a certain level of sarcasm.');
+  } else {
+    alert('Dude! WTF is that? Yes or no. It\'s not that complicated.');
+  }
+  console.log('It\'s her first language.');
 }
-console.log('It\'s her first language.');
+sarcasm();
 
-var blackBirds = prompt('Does Sharon like large black birds like crows & ravens?');
-if (blackBirds.toLowerCase() === 'yes' || blackBirds.toLowerCase() === 'y' ) {
-  alert('That\'s right. She regards them as something of a spirit animal.');
-} else if (blackBirds.toLowerCase() === 'no' || blackBirds.toLowerCase() === 'n' ){
-  alert('She certainly does! She regards them as something of a spirit animal.');
-} else {
-  alert('Dude! WTF is that? Yes or no. It\'s not that complicated');
+//Question 2 - Birds
+function blackBirds() {
+  var blackBirds = prompt('Do I like large black birds like crows & ravens?');
+  if (blackBirds.toLowerCase() === 'yes' || blackBirds.toLowerCase() === 'y' ) {
+    alert('That\'s right. I regard them as something of a spirit animal.');
+    totalCorrect++;
+  } else if (blackBirds.toLowerCase() === 'no' || blackBirds.toLowerCase() === 'n' ){
+    alert('I certainly do! I regard them as something of a spirit animal.');
+  } else {
+    alert('Dude! WTF is that? Yes or no. It\'s not that complicated.');
+  }
+  console.log('*starts humming the Beatle\'s "Blackbird"*');
 }
-console.log('*starts humming the Beatle\'s "Blackbird"*');
+blackBirds();
 
-var wombatFan = prompt('Does Sharon also love wombats?');
-if (wombatFan.toLowerCase() === 'yes' || wombatFan.toLowerCase() === 'y' ) {
-  alert('Yes indeed, ' + userName + '. She even has 2 stuffed wombats she considers her "pets". Their names are Watson & Wanda');
-} else if (wombatFan.toLowerCase() === 'no' || wombatFan.toLowerCase() === 'n' ){
-  alert('Oh, she does, ' + userName + '. She does. It\'s all North Dakota\'s fault.');
-} else {
-  alert('Dude! WTF is that? Yes or no. It\'s not that complicated.');
+//Question 3 - Wombats
+function wombat() {
+  var wombatFan = prompt('Do I also love wombats?');
+  if (wombatFan.toLowerCase() === 'yes' || wombatFan.toLowerCase() === 'y' ) {
+    alert('Yes indeed, ' + userName + '. I even have 2 stuffed wombats I consider my "pets". Their names are Watson & Wanda & they\'re adorable. And they don\'t eat much.');
+    totalCorrect++;
+  } else if (wombatFan.toLowerCase() === 'no' || wombatFan.toLowerCase() === 'n' ){
+    alert('Oh, I do, ' + userName + '. I do. It\'s all North Dakota\'s fault.');
+  } else {
+    alert('Dude! WTF is that? Yes or no. It\'s not that complicated.');
+  }
+  console.log('May the Wombat of Happiness shuffle through your underbrush.');
 }
-console.log('May the Wombat of Happiness shuffle through your underbrush.');
+wombat();
 
-var radioDJ = prompt('Was Sharon a college radio DJ?');
-if (radioDJ.toLowerCase() === 'yes' || radioDJ.toLowerCase() === 'y' ) {
-  alert('Did you ever hear her show, ' + userName + '?');
-} else if (radioDJ.toLowerCase() === 'no' || radioDJ.toLowerCase() === 'n' ){
-  alert('She was! She DJ\'d on WERG in Erie, PA back in the early 90\'s');
-} else {
-  alert('Dude! WTF is that? Yes or no. It\'s not that complicated');
+// Question 4 - DJ
+function radio() {
+  var radioDJ = prompt('Was I a college radio DJ?');
+  if (radioDJ.toLowerCase() === 'yes' || radioDJ.toLowerCase() === 'y' ) {
+    alert('Yep! My air name was Molly McQuire. I took the name from a band of Irish miners rights activist/terrorists (depending on who you ask) that were tried & hung in my Dad\s hometown of Pottsville, PA.');
+    totalCorrect++;
+  } else if (radioDJ.toLowerCase() === 'no' || radioDJ.toLowerCase() === 'n' ){
+    alert('I was! I DJ\'d on WERG in Erie, PA back in the early 90\'s. Fun fact- WERG was the station for a different university than the one I was actually attending.');
+  } else {
+    alert('Dude! WTF is that? Yes or no. It\'s not that complicated.');
+  }
+  console.log('Nothin\' but the local DJ. Said she had a song to play. What came down from her foolin\' around gave us hope for a brand new day');
 }
-console.log('This request goes out to my boys at the BP on I-90');
+radio();
 
-var heavyReader = prompt('Does Sharon hate to read?');
-if (heavyReader.toLowerCase() === 'yes' || heavyReader.toLowerCase() === 'y' ) {
-  alert('Wrong! Wrong! Wrong! You\'ve obviously never been to her apartment, ' + userName + '.');
-} else if (heavyReader.toLowerCase() === 'no' || heavyReader.toLowerCase() === 'n' ){
-  alert( 'She reads constantly! She even picked up an e-reader because she ran out of room in her apartment for more shelves.');
-} else {
-  alert('Dude! WTF is that? Yes or no. It\'s not that complicated');
+// Question 5 - Reading
+function reader() {
+  var heavyReader = prompt('Do I hate to read?');
+  if (heavyReader.toLowerCase() === 'yes' || heavyReader.toLowerCase() === 'y' ) {
+    alert('Wrong! Wrong! Wrong! You\'ve obviously never been to my apartment, ' + userName + '.');
+    totalCorrect++;
+  } else if (heavyReader.toLowerCase() === 'no' || heavyReader.toLowerCase() === 'n' ){
+    alert( 'I read constantly! I even picked up an e-reader because I ran out of room in my apartment for more shelves.');
+  } else {
+    alert('Dude! WTF is that? Yes or no. It\'s not that complicated');
+  }
+  console.log('I like big books and I cannot lie.');
 }
-console.log('I like big books and I cannot lie.');
-
+reader();
 
 // Wednesday Lab Additions
 // Number Guess
 
-var numberGuess = prompt('How many semesters did Sharon teach Jewelry/Metalsmithing at UND?');
-var numberGuessI = parseInt(numberGuess);
-var teachSemesters = 6;
+// Question 6 - Teaching
+function teaching() {
+  var teachSemesters = 6;
 
-if (numberGuessI === teachSemesters) {
-  alert('That\'s right! She did!');
-} else if (numberGuessI < teachSemesters) {
-  alert('Not quite. She taught more than ' + numberGuess + '.');
-} else if (numberGuessI > teachSemesters) {
-  alert('Alas, Sharon didn\'t teach quite that many');
-} else {
-  alert('That\'s not a number, silly!');
+  for (var i = 4; i >= 1 && degreesQ !== degreesA; i--) {
+    var numberGuess = prompt('How many semesters did I teach Jewelry/Metalsmithing at UND?');
+    var numberGuessInt = parseInt(numberGuess);
+
+    if (numberGuessInt === teachSemesters) {
+      alert('That\'s right! I did!');
+      totalCorrect++;
+    } else if (numberGuessInt < teachSemesters) {
+      alert('Not quite. I taught more than ' + numberGuess + '.');
+    } else if (numberGuessInt > teachSemesters) {
+      alert('Alas, I didn\'t teach quite that many.');
+    } else {
+      alert('I taught for 6 semesters & really loved it. ');
+    }
+    console.log('Even when my students drove me to drink!');
+  }
 }
-console.log('You guessed ' + numberGuessI + '. She taught for ' + teachSemesters + ' semesters. She loved it!');
+teaching();
+
+// Question 7 - Home
+function home() {
+  var cityA = ['erie', 'baltimore', 'union city', 'grand forks', 'salem', 'portland'];
+  var correct = false;
+  for (var i = 5; i >= 0 && correct != true; i--) {
+    var cityQ = prompt('Can you guess a city that I\'ve lived in? You have ' + (i + 1) + ' guesses.').toLowerCase();
+    if (cityQ == cityA[0] || cityQ == cityA[1] || cityQ == cityA[2] || cityQ == cityA[3] || cityQ == cityA[4] || cityQ == cityA[5]) {
+      alert('That\'s right!');
+      totalCorrect++;
+      correct = true;
+    } else {
+      alert('Nope. I\'ve never lived in ' + cityQ);
+    }
+    console.log('I did not die of cholera.');
+  }
+  alert('I\'ve lived in Erie, PA; Baltimore, MD; Union City, PA; Grand Forks, ND; Portland, OR; and Salem, OR. I\'m currently in Portland & I love it!');
+}
+home();
+
+if (totalCorrect === 7) {
+  alert('You got every question right! You know me well. Possibly too well.');
+} else if (totalCorrect >= 5) {
+  alert('You got ' + totalCorrect + ' answers correct. We\'ve obviously hung out a few times.');
+} else if (totalCorrect >= 3) {
+  alert('You got ' + totalCorrect + ' answers correct. You knda sorta know me a bit.');
+} else if (totalCorrect >= 1){
+  alert('You got ' + totalCorrect + ' answers correct. You are confused.');
+} else if (totalCorrect === 0){
+  alert('You didn\'t get any correct answers. You haven\'t even met me. Admit it. You were looking for an entirely different person named Sharon Smith when you found this site');
+} else {
+  alert('Squirrel!');
+}
